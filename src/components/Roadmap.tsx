53bc58207d429f925f2da6b795c6f6bc9f9bd790
Akhilesh Balaji <akhilesh.balaji.bangalore@gmail.com>
2026-05-29 12:35:00 +0530

    changed pre styling slightly

--- src/components/Roadmap.tsx
+++ src/components/Roadmap.tsx
@@ -122,8 +122,8 @@ export default function Roadmap() {
           prose-p:text-base-content prose-p:mb-3 md:prose-p:text-lg
           prose-a:text-primary prose-a:no-underline hover:prose-a:underline
           prose-code:text-secondary prose-code:bg-base-300 prose-code:px-1 prose-code:rounded
-          prose-pre:bg-transparent! prose-pre:p-2! prose-pre:m-0! prose-pre:rounded-lg!
-          prose-pre:text-lg prose-pre:border-base-300 prose-pre:border-2
+          prose-pre:bg-base-300/7! prose-pre:p-2! prose-pre:m-0! prose-pre:rounded-lg!
+          prose-pre:text-lg prose-pre:border-base-300/25 prose-pre:border-2
           prose-strong:text-base-content
           md:prose-table:text-lg
           prose-li:text-base-content prose-li:text-lg
